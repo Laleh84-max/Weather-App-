92574b37d86f20983cf0b1ce40999d5e75fe6066
Laleh84-max <Laleh.narimisa@yahoo.com>
2024-04-19 17:47:07 +0200

    added some css

--- src/app.js
+++
@@ -1,9 +0,0 @@
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  let temperature = Math.round(response.data.temperature.current);
-  temperatureElement.innerHTML = temperature;
-}
-let city = "Sydney";
-let apiKey = "0adcb4fbfo8d4aa6a3f1c1c5t607f7eb";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-axios.get(apiUrl).then(displayTemperature);
