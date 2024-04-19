8f6c9080ce0052af5c7f80d0311a095fd89c62c4
Laleh84-max <Laleh.narimisa@yahoo.com>
2024-04-19 11:31:55 +0200

    added API

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
+function displayUsername(response) {
+  console.log(response.data.username);
+}
+
+let apiUrl = "https://jsonplaceholder.typicode.com/users/1";
+axios.get(apiUrl).then(displayUsername);
+
 function search(event) {
   event.preventDefault();
   let searchInput = documet.querySelector("#search-value-input");
