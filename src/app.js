254f031f5d2bfd9ab5ce4761e57e11b978d07df9
Laleh84-max <Laleh.narimisa@yahoo.com>
2024-04-19 12:29:46 +0200

    calling data

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 function displayUsername(response) {
-  console.log(response.data.username);
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `Hey ${response.data.username}`;
 }
 
 let apiUrl = "https://jsonplaceholder.typicode.com/users/1";
