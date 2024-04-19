0c2f8ecfb4a9ce45d01d795efccd8513cd8446d8
Laleh84-max <Laleh.narimisa@yahoo.com>
2024-04-19 11:01:58 +0200

    added axios to html

---
+++ src/app.js
@@ -0,0 +1,7 @@
+function search(event) {
+  event.preventDefault();
+  let searchInput = documet.querySelector("#search-value-input");
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
